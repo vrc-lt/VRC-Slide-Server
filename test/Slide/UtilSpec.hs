f8075beedf60b1433030f9c5b0aba66f7dc74dbc
boxp <tiyotiyouda@gmail.com>
2019-02-15 00:00:36 +0900

    getSlideIdのテスト追加

--- test/Slide/UtilSpec.hs
+++ test/Slide/UtilSpec.hs
@@ -10,10 +10,10 @@ spec :: Spec
 spec = do
   describe "getSlideId" $ do
     it "表示するページが0の時、一つめのスライドIDが返る" $ do
-          getSlideId slideIds slideNums 0 `shouldBe` "id1"
+          getSlideId slideIds slideNums 0 `shouldBe` Just "id1"
     it "表示するページが12以下の時、一つめのスライドIDが返る" $ do
-      getSlideId slideIds slideNums 12 `shouldBe` "id1"
+      getSlideId slideIds slideNums 12 `shouldBe` Just "id1"
     it "表示するページが13の時、２つめのスライドIDが返る" $ do
-      getSlideId slideIds slideNums 13 `shouldBe` "id2"
+      getSlideId slideIds slideNums 13 `shouldBe` Just "id2"
     it "表示するページが14以上42未満の時、２つめのスライドIDが返る" $ do
-      getSlideId slideIds slideNums 13 `shouldBe` "id2"
+      getSlideId slideIds slideNums 13 `shouldBe` Just "id2"
